4f11905fd112740daea9015f5d55dc12544a15a2
sonu-kumar2001 <sonu2202kumar@gmail.com>
2023-02-08 23:18:43 +0530

    implemented admin panel

--- routes/v1/admin.routes.js
+++ routes/v1/admin.routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const activityController = require("../../controllers/activity.controller");
+const auth = require("./authentication/authenticate");
 
-router.get("/activity/details", activityController.getActivityDetails);
+router.get(
+  "/activity/details",
+  auth.verifyToken,
+  activityController.getActivityDetails
+);
 
 module.exports = router;
